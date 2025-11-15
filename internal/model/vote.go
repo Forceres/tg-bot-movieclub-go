cb2fe189ec913416829c07642e47c6f2507ef635
Forceres <forceres31@gmail.com>
2025-11-15 05:10:39 +0400

    refactor: delete index in vote model

--- internal/model/vote.go
+++ internal/model/vote.go
@@ -4,11 +4,11 @@ import "gorm.io/gorm"
 
 type Vote struct {
 	gorm.Model
-	ID       int64  `gorm:"primaryKey"`
-	VotingID int64  `gorm:"index:,unique,composite:idx_user_voting"`
+	ID       int64 `gorm:"primaryKey"`
+	VotingID int64
 	Voting   Voting `gorm:"foreignKey:VotingID"`
-	UserID   int64  `gorm:"index:,unique,composite:idx_user_voting"`
-	User     User   `gorm:"foreignKey:UserID"`
+	UserID   int64
+	User     User `gorm:"foreignKey:UserID"`
 	MovieID  *int64
 	Movie    *Movie `gorm:"foreignKey:MovieID"`
 	Rating   *int   `gorm:"check:rating >= 1 AND rating <= 10"`
